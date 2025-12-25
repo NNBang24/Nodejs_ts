7ed96bc7d9122c135052f76cab2644070890ce48
Bang <nnb24.112003@gmail.com>
2025-12-26 01:34:10 +0700

    cap nhat hoc (routes , env(Environmenr Variables) , Template(View) Engine

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
-const express = require('express') ;
-const app = express() ;
-const PORT = 3000 ;
+import 'dotenv/config' ;
+// require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get('/' ,(req ,res) => {
-  res.send("hello word")
+// config view engine 
+app.set('view engine', 'ejs') ;
+app.set('views',__dirname +'/views')
+app.get('/' ,(req, res) => {
+  res.render("home.ejs")
+})
+
+app.get('/', (req, res) => {
+  res.send(`<h1 style="color: red">hello word</h1>`)
+
+});
+app.get('/bang', (req, res) => {
+  res.send("hello Bang")
 
 });
 
-app.listen(PORT , () => {
-    console.log(`lang nghe tai http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`lang nghe tai http://localhost:${PORT}`)
+  console.log("env prot :", process.env.PORT)
+});
